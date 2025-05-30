0fbbaba9b17d8ea6879c152265c091609f546ba1
nawszin <kkowalskigustavo@gmail.com>
2025-05-30 13:55:46 -0300

    Projeto Criado

---
+++ src/main/java/com/devsuperior/CRUDesafio/CruDesafioApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.CRUDesafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CruDesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruDesafioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/CRUDesafio/CruDesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.CRUDesafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruDesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
