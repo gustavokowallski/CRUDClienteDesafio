32ec2ef729e03fd6e164e5729635a39d664986af
nawszin <kkowalskigustavo@gmail.com>
2025-05-30 22:15:35 -0300

    Camada Clients implementada

--- src/main/java/com/devsuperior/CRUDesafio/dtos/ClientDTO.java
+++ src/main/java/com/devsuperior/CRUDesafio/dtos/ClientDTO.java
@@ -1,4 +1,51 @@
 package com.devsuperior.CRUDesafio.dtos;
 
+import com.devsuperior.CRUDesafio.entities.Client;
+
+import java.time.LocalDate;
+
 public class ClientDTO {
+    private Long id;
+    private String name;
+    private String cpf;
+    private Double income;
+    private LocalDate bithDate;
+    private Integer childer;
+
+    public ClientDTO(Client client) {
+        id = client.getId();
+        name = client.getName();
+        cpf = client.getCpf();
+        income = client.getIncome();
+        bithDate = client.getBithDate();
+        childer = client.getChilder();
+
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Integer getChilder() {
+        return childer;
+    }
+
+    public LocalDate getBithDate() {
+        return bithDate;
+    }
+
+    public Double getIncome() {
+        return income;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
 }
